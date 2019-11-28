29f5156f1425c840862b6c9c1f49d349bc0a2339
kuokuo666 <2721791961@qq.com>
2019-11-28 16:49:03 +0800

    2019-11-28

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 ## CocosCreator2.x编写
  分享一些自己写的 CocosCreator 小脚本插件。可直接挂载使用。觉得不错给个星吧 O(∩_∩)O 。
 ## 目录
+- joystick 摇杆小组件，拖入回调接收vector，angle两个参数。
 - move-limit 移动限位，两种模式固定节点移动区域，挂载在节点上。
 - touch-move 触摸移动，有两种模式，可以挂载在画布上，例如用于飞行射击游戏。
 - touch-events 组件化四个触摸事件，拖拽使用。
---
+++ joystick.js
@@ -0,0 +1,89 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        /** 摇杆移动中心 */
+        midNode: {
+            default: null,
+            type: cc.Node,
+            displayName: '移动中心节点'
+        },
+        /** 摇杆背景做监听，体验好些 */
+        joyBk: {
+            default: null,
+            type: cc.Node,
+            displayName: '摇杆背景节点'
+        },
+        /** 摇杆最大移动半径 */
+        maxR: {
+            default: 100,
+            displayName: '摇杆活动半径'
+        },
+        /** 摇杆移动回调 */
+        joyCallBack:  {
+            default: [],
+            type: cc.Component.EventHandler,
+            displayName: '摇杆移动回调',
+            tooltip: '触发touchmove后分发数据'
+        }
+    },
+
+    onLoad () {
+        // 归位
+        this.goBackMid();
+    },
+
+    start () {
+        this.joyBk.on(cc.Node.EventType.TOUCH_START, this.onTouchStart, this);
+        this.joyBk.on(cc.Node.EventType.TOUCH_MOVE, this.onTouchMove, this);
+        this.joyBk.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+        this.joyBk.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+    },
+
+    /** 回归中心 */
+    goBackMid () {
+        this.joyBk.setPosition(0, 0);
+        this.midNode.setPosition(0, 0);
+    },
+
+    onTouchStart (e) {
+        let pos = this.node.convertToNodeSpaceAR(e.getLocation());
+        this.clampPos(pos);
+        this.midNode.setPosition(pos.x, pos.y);
+        let angle = this.covertToAngle(pos);
+        // 触发回调，参数类型 Vec2，number
+        this.joyCallBack.emit([pos, angle]);
+    },
+
+    onTouchMove (e) {
+        let pos = this.node.convertToNodeSpaceAR(e.getLocation());
+        this.clampPos(pos);
+        this.midNode.setPosition(pos.x, pos.y);
+        let angle = this.covertToAngle(pos);
+        // 触发回调，参数类型 Vec2，number
+        this.joyCallBack.emit([pos, angle]);
+    },
+
+    onTouchEnd (e) {
+        this.goBackMid();
+        this.joyCallBack.emit([cc.v2(0, 0)]);
+    },
+
+    /** 根据半径限制位置 */
+    clampPos (pos) {
+        let len = pos.mag();
+        if (len > this.maxR) {
+            let k = this.maxR / len;
+            pos.x *= k;
+            pos.y *= k;
+        }
+    },
+
+    /** 根据位置转化角度 */
+    covertToAngle (pos) {
+        let r = Math.atan2(pos.y, pos.x);
+        let d = cc.misc.radiansToDegrees(r);
+        return d;
+    },
+
+});
